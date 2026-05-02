d4b396df73bac3a427c9110f27ce43b142c8fcae
willanisaac <willan.tuquerrez@outlook.com>
2026-05-02 12:14:42 -0500

    chore(vite.config): actualizar configuracion

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,25 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-export default defineConfig({
-  plugins: [svelte()],
-});
+export default defineConfig(({ mode }) => {
+  // Carga las variables de entorno desde .env
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [svelte()],
+    base: '/vexal/',
+    server: {
+      port: 3000,
+      open: true,
+      allowedHosts: [
+        'kapisg.com',
+        'kapisg.com/vexal'
+      ]
+    },
+    define: {
+      // Expone las variables de entorno al código del cliente
+      'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(env.VITE_SUPABASE_URL),
+      'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(env.VITE_SUPABASE_ANON_KEY),
+    }
+  }
+})
